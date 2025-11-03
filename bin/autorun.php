a67fa0392064e35e24ba8e5a6c664333aa0baa0e
Guest <kid666@88.com>
2025-11-03 17:39:31 +0800

    调整与pms-basic的关系

--- bin/autorun.php
+++ bin/autorun.php
@@ -1,21 +1,22 @@
 <?php
-\pms\hook\LifecycleHook::mount(function () {
-    $dbConfig = config('redis');
-    if ($dbConfig !== null) {
-        \pms\facade\RDb::setConfig($dbConfig);
-    }
-});
-
+if (class_exists('\pms\hook\LifecycleHook')) {
+    \pms\hook\LifecycleHook::mount(LIFECYCLE_BOOT, function () {
+        $dbConfig = config('redis');
+        if ($dbConfig !== null) {
+            \pms\facade\RDb::setConfig($dbConfig);
+        }
+    });
+}
 if (in_swoole()) {
-    if (defined(SWOOLE_HTTP_LIFECYCLE_START)) {
-        \pms\hook\SwooleHttpLifecycleHook::mount(SWOOLE_HTTP_LIFECYCLE_START, function () {
-            \pms\facade\RDb::isPool( true);
+    if (class_exists('\pms\hook\SwooleHttpLifecycleHook')) {
+        \pms\hook\SwooleHttpLifecycleHook::mount(LIFECYCLE_BOOT, function () {
+            \pms\facade\RDb::isPool(true);
         });
-        \pms\hook\SwooleHttpLifecycleHook::mount(SWOOLE_HTTP_LIFECYCLE_REQUEST_DESTRUCT, function () {
+        \pms\hook\SwooleHttpLifecycleHook::mount(SWOOLE_LIFECYCLE_HTTP_REQUEST_DESTRUCT, function () {
             try {
                 prdb_pool_autoclose();
             } catch (\Throwable $e) {
-                echo "Redis连接池归还错误：" . $e->getMessage() . "\r\n";
+                echo "Redis连接池错误：" . $e->getMessage() . "\r\n";
             }
         });
     }
